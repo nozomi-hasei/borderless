9a4c07042b010b39fa602c0ce29e0599c8d03e1b
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-17 14:49:19 +0900

    タグ投稿

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,17 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:text_tag, :text_memo)
+  end
 end
