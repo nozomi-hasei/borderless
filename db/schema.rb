46a65079e76706b5a8784e46aea04ed6cc7986ef
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-17 14:21:30 +0900

    TOPページ　投稿一覧表示

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_145636) do
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text_tag", null: false
     t.string "text_memo", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
