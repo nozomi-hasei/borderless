8c6559c489ab4510ae97ee8eb3364b9a052a02b1
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-18 12:19:16 +0900

    Revert "Revert "tagsテーブル""
    This reverts commit b66802f180cb0214cded89a56cc5f87a30f47fc6.

---
+++ db/migrate/20210516145636_create_tags.rb
@@ -0,0 +1,9 @@
+class CreateTags < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tags do |t|
+      t.string :text_tag, null:false
+      t.string :text_memo, null:false
+      t.timestamps
+    end
+  end
+end
