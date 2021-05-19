cfe009635ec4b8c6fc1d2eb8a382fdff451942fa
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-19 23:55:25 +0900

    new createアクション　ビュー

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,11 @@
 class ProjectsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: :show
-  resources :projects, only: :index
+  resources :projects, only: [:index, :new, :create]
 end
