2517c4d6553bc46085261c734cabe1704c2d74f6
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-19 23:51:31 +0900

    プロジェクトモデル　バリデーション

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ApplicationRecord
+  validates :main_title,      presence: true
+  validates :start_date,      presence: true
+  validates :finish_date,     presence: true
+  validates :department,      presence: true
+  validates :category,        presence: true
+  validates :project_memo,    presence: true
+  
   belongs_to :user
   has_many :comments
   has_one_attached :image
