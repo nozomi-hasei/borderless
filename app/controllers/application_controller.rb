99dab110e7bbe02b7111bd513e0f298565edbfb4
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-07-04 22:08:29 +0900

    ログイン・ログアウト後のページ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    tags_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   private
   def configure_permitted_parameters
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def after_sign_in_path_for(resource)
+    tags_path(resource)
+  end
+  
   def show
     if user_signed_in? && current_user.id 
       @nickname = current_user.nickname
